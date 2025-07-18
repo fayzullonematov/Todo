7ebaf0ef0ad926af1592accdf2662e298caeafaf
fayzullonematov <fayzullo2007nematov@gmail.com>
2025-07-18 19:10:25 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const button = document.querySelector("button");
+const input = document.querySelector("#todo-input");
+const ul = document.querySelector("#todo-list");
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  const text = input.value;
+  const task = document.createElement("li");
+  const deltbtn = document.createElement("button");
+  const done = document.createElement("button");
+
+  const span = document.createElement("span");
+  span.textContent = text;
+
+  deltbtn.textContent = "❌";
+  deltbtn.addEventListener("click", function () {
+    task.remove();
+  });
+
+  done.textContent = "✅";
+  done.addEventListener("click", function () {
+    span.classList.toggle("lista");
+  });
+
+  task.appendChild(span);
+  task.appendChild(deltbtn);
+  task.appendChild(done);
+  ul.appendChild(task);
+
+  input.value = "";
+});
